034959a8596caef13fc8faf3cfeebf800ead7a68
satvshr <112589278+satvshr@users.noreply.github.com>
2023-04-14 11:43:03 +0530

    changed sidebar.js

--- src/pages/components/Sidebar.js
+++ src/pages/components/Sidebar.js
@@ -1,12 +1,22 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import CreateEventButton from './CreateEventButton';
 import SmallCalendar from './SmallCalendar';
+import { useUser } from '@auth0/nextjs-auth0/client';
 
 export default function Sidebar() {
+  const { user } = useUser();
+  const [isSuperUser, setSuperUser] = useState(false);
+
+  useEffect(() => {
+    if (user && user.name === 'ivinjoelabraham@outlook.com') {
+      setSuperUser(true);
+    }
+  }, [user])
+
   return (
     <div>
       <aside className='border p-5 w-64'>
-        <CreateEventButton />
+        {isSuperUser && <CreateEventButton />}
         <SmallCalendar />
       </aside>
     </div>
